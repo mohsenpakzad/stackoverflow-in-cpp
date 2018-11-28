14cdbdbc0c369909ccfe71095cc228904706facb
Mohsen Pakzad <mohsen137853@gamil.com>
2018-11-28 19:31:08 +0330

    Change main function design and design new loggedin user menu

--- GeneralFunctions.cpp
+++ GeneralFunctions.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+#include"GeneralFunctions.h"
 
 
 void cleanBuf() { // Don't you ever think user might be stupic!?
 	while (getchar() != '\n');
+}
+
+void showMessageToUser(const char * message) {
+	system(CLEAR);
+	std::cout << message << std::endl;
+	SystemPause;
 }
