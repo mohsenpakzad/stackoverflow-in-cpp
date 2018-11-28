b1f497f3512804700206df4b33cd16507b5814f4
Mohsen Pakzad <mohsen137853@gamil.com>
2018-11-28 15:52:05 +0330

    update Content.h and ContentRelation.h and add prototypes

--- Content.h
+++ Content.h
@@ -1,15 +1,29 @@
 #pragma once
 
-#include<iostream>
+#include<vector>
 #include<string>
+#include"ContentRelation.h"
 
+
+using namespace std;
+
+enum ContentType {
+	QUESTION,
+	ANSWER
+};
 
 class Content {
 
+private:
+
+	string * topic; // topic is only for question, it must allocated only for question!
+	string body;
+
+	int visits;
+	ContentType type;
+	vector<ContentRelation> relations;
+
 public:
-
-	std::string body;
-
-
+	
 
 };
--- ContentRelation.h
+++ ContentRelation.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include"Content.h"
+
+enum ContentRelationType {
+	DUPLICATE_OF,
+	ANSWER_TO
+};
+
+class ContentRelation {
+
+
+
+};
